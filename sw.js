049d7593897a1808cf92a95cef5262bea35ce4e7
fushan345 <fushancen@gmail.com>
2026-07-21 03:57:26 +0800

    Refresh offline cache for font update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "excerpt-drawer-v1";
+const CACHE_NAME = "excerpt-drawer-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
