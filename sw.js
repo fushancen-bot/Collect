7a3ddd04b71d6f484651b8b6b8dbcaebe6496b5d
fushan345 <fushancen@gmail.com>
2026-07-21 04:45:34 +0800

    Simplify entry field styling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "excerpt-drawer-v6";
+const CACHE_NAME = "excerpt-drawer-v7";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
