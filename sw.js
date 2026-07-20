3c4bc258e629558c7bd595ba8536fb9b37487e70
fushan345 <fushancen@gmail.com>
2026-07-21 04:35:29 +0800

    Make source details optional

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "excerpt-drawer-v4";
+const CACHE_NAME = "excerpt-drawer-v5";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
