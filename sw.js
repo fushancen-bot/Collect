d9e4e4133608602740e250205ece37f0d8e4ac77
fushan345 <fushancen@gmail.com>
2026-07-21 04:08:07 +0800

    Refresh cache for typography update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "excerpt-drawer-v2";
+const CACHE_NAME = "excerpt-drawer-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
