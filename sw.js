33e545417d41dfb60727994bb608cbd75e04fdb3
fushan345 <fushancen@gmail.com>
2026-07-21 04:50:40 +0800

    Restore rounded entry fields

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "excerpt-drawer-v7";
+const CACHE_NAME = "excerpt-drawer-v8";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
